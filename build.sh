392ba370b0d299c35387baefe890f66031ce134b
mallikarjun arjun <kmalleshmallesh5432@gmail.com>
2026-05-06 11:16:12 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Building for Render..."
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+echo "Build complete."
