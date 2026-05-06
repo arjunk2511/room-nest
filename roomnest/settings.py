4b8ae20a6f72f6a22d83a1ff1169b585bf437c6e
mallikarjun arjun <kmalleshmallesh5432@gmail.com>
2026-05-06 13:29:06 +0530

    Add files via upload

--- roomnest/settings.py
+++ roomnest/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-dummy-key-for-roomnest-project'
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'RENDER' not in os.environ or os.environ.get('FORCE_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     
     # Custom apps
     'accounts.apps.AccountsConfig',
@@ -54,12 +56,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'roomnest.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Override database on Render (Production) to use PostgreSQL
+if 'RENDER' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -85,6 +96,22 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
+# Legacy settings required by django-cloudinary-storage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+# Prevent WhiteNoise from crashing on missing static files in CSS
+WHITENOISE_MANIFEST_STRICT = False
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -100,3 +127,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 # Reload trigger
+
+# Cloudinary credentials (add these to Render Environment Variables)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'your_cloud_name_here'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', 'your_api_key_here'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', 'your_api_secret_here'),
+}
